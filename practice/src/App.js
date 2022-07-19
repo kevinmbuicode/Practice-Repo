cab73449e46bcc9aec4f993dd9ff0d594bcc88d9
kevinmbuicode <kevinmushan@gmail.com>
2022-07-19 17:09:39 +0300

    Switch Favorite | not Favorite star icon achieved

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,22 +1,61 @@
-import Contacts from './components/Contact';
+import React from "react";
+//import Contacts from './components/Contact';
 //import Jokes from './components/Jokes';
 //import jokesData from  './jokesData'
-import Mocks from './MockData'
+//import Mocks from './MockData'
+import catImage from "./components/images/cat-four.png";
+import starEmptyIcon from "./components/images/star-empty.webp"
+import starFilledIcon from "./components/images/star-full-icon.png"
+import './index.css'
 
 function App() {
-  const data = Mocks.map(mock => {
-    return(
-      <Contacts
-        id = {mock.id}
-        mock = {mock}
-       />
-    )
+
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Cena",
+    phone: "+ 1 (819) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false
   })
 
+  function toggleFavorite() {
+    console.log("Toogle Favorite Clicked")
+    setContact(prevContact => ({
+      ...prevContact,
+      isFavorite: !prevContact.isFavorite
+    }))
+  }
+
+  let starIcon = (contact.isFavorite ? starFilledIcon : starEmptyIcon)
+  
   return (
-    <div className="App">
-      {data}
-    </div>
+    <main>
+      <article className="card">
+        <img src={catImage}  
+        alt="contact_image" 
+        className="card--image" 
+        height='150px' width='250px'/>
+
+        <div className="card--info">
+          <img src={starIcon} 
+          onClick={toggleFavorite}
+          alt="star icon" 
+          height='18px' width='18px'/>
+
+          <h2 
+          className="card--name">
+
+            {contact.firstName} {contact.lastName}
+          </h2>
+
+          <p className="card--contact">
+          {contact.phone}</p>
+
+          <p className="card--contact">
+          {contact.email}</p>
+        </div>
+      </article>
+    </main>
     
   );
 }
