a48ca18324b89af58a3a7d44160ce720da22b8c3
kevinmbuicode <kevinmushan@gmail.com>
2022-09-23 19:01:20 +0300

    Use Ref hook, what, why, how answered

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,90 +1,116 @@
-import React from "react";
+import React, { useState, useEffect, useRef} from "react";
 //import Contacts from './components/Contact';
 //import Jokes from './components/Jokes';
 //import jokesData from  './jokesData'
 //import Mocks from './MockData'
-import catImage from "./components/images/cat-four.png";
-import starEmptyIcon from "./components/images/star-empty.webp"
-import starFilledIcon from "./components/images/star-full-icon.png"
-import StarIcon from "./components/StarIcon";
-import './index.css'
-import WelcomeUser from "./components/Welcome";
+// import catImage from "./components/images/cat-four.png";
+// import starEmptyIcon from "./components/images/star-empty.webp"
+// import starFilledIcon from "./components/images/star-full-icon.png"
+// import StarIcon from "./components/StarIcon";
+// import './index.css'
+// import WelcomeUser from "./components/Welcome";
 
 
 
 
 function App() {
-  const [count, setCount] = React.useState(0)
-
-  const [contact, setContact] = React.useState({
-    firstName: "John",
-    lastName: "Cena",
-    phone: "+ 1 (819) 555-1212",
-    email: "itsmyrealname@example.com",
-    isFavorite: false
+  const [ name, setName] = useState("")
+  const render = useRef(0)
+  const inputRef = useRef()
+  // use Ref looks like this { current: 0 because we set our useRef to 0}
+  useEffect(()=> {
+    render.current = render.current + 1
   })
-
-  function toggleFavorite() {
-    console.log("Toogle Favorite Clicked")
-    setContact(prevContact => ({
-      ...prevContact,
-      isFavorite: !prevContact.isFavorite
-    }))
+  function focus() {
+    inputRef.current.focus()
   }
 
-  let starIcon = (contact.isFavorite ? starFilledIcon : starEmptyIcon)
+
+  //--------------------------------------------------------------------------------------
+  //  Put this code below to return to how it was
+  // -------------------------------------------------------------------------------------
+
+  // const [count, setCount] = React.useState(0)
+
+  // const [contact, setContact] = React.useState({
+  //   firstName: "John",
+  //   lastName: "Cena",
+  //   phone: "+ 1 (819) 555-1212",
+  //   email: "itsmyrealname@example.com",
+  //   isFavorite: false
+  // })
+
+  // function toggleFavorite() {
+  //   console.log("Toogle Favorite Clicked")
+  //   setContact(prevContact => ({
+  //     ...prevContact,
+  //     isFavorite: !prevContact.isFavorite
+  //   }))
+  // }
+
+  // let starIcon = (contact.isFavorite ? starFilledIcon : starEmptyIcon)
   
-  function countUP() {
-    console.log("Up clicked")
-    setCount(prevCount => prevCount + 1)
-  }
+  // function countUP() {
+  //   console.log("Up clicked")
+  //   setCount(prevCount => prevCount + 1)
+  // }
 
-  function countDown() {
-    console.log("Down clicked")
-    setCount(prevCount => prevCount - 1)
-  }
+  // function countDown() {
+  //   console.log("Down clicked")
+  //   setCount(prevCount => prevCount - 1)
+  // }
+
+  //--------------------------------------------------------------------------------------
+  // // Put this code below to return to how it was
+  // -------------------------------------------------------------------------------------
+  // <main>
+  //   <WelcomeUser user={contact.firstName}/>
+  //     <article className="card">
+  //       <img src={catImage}  
+  //       alt="contact_image" 
+  //       className="card--image" 
+  //       height='150px' width='250px'/>
+
+  //       <div className="card--info">
+  //         <StarIcon
+  //           src = {starIcon}
+  //           isFilled = {contact.isFavorite}
+  //           toggle = {toggleFavorite}
+  //         />
+
+  //         <h2 
+  //         className="card--name">
+
+  //           {contact.firstName} {contact.lastName}
+  //         </h2>
+
+  //         <p className="card--contact">
+  //         {contact.phone}</p>
+
+  //         <p className="card--contact">
+  //         {contact.email}</p>
+  //         {/* {passing state as props} */}
+  //       </div>
+          
+  //     </article>
+
+  //     <button onClick={countUP}>
+  //           Add
+  //     </button>
+  //     {count}
+  //     <button onClick={countDown}>
+  //       Subtract
+  //     </button>
+  //   </main>
 
 
   return (
-    <main>
-    <WelcomeUser user={contact.firstName}/>
-      <article className="card">
-        <img src={catImage}  
-        alt="contact_image" 
-        className="card--image" 
-        height='150px' width='250px'/>
-
-        <div className="card--info">
-          <StarIcon
-            src = {starIcon}
-            isFilled = {contact.isFavorite}
-            toggle = {toggleFavorite}
-          />
-
-          <h2 
-          className="card--name">
-
-            {contact.firstName} {contact.lastName}
-          </h2>
-
-          <p className="card--contact">
-          {contact.phone}</p>
-
-          <p className="card--contact">
-          {contact.email}</p>
-          {/* {passing state as props} */}
-        </div>
-          
-      </article>
-
-      <button onClick={countUP}>
-            Add
-      </button>
-      {count}
-      <button onClick={countDown}>
-        Subtract
-      </button>
-    </main>
+    <>
+      <input ref={inputRef} value={name}type="text" onChange={(e) => setName(e.target.value)} />
+      <p>My name is {name}</p>
+      <p>I rendered {render.current} times</p>
+      <button onClick={focus}>Focus</button>
+    </>
     
   );
 }
